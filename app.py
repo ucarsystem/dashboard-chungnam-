31adbcec69a9cccad90788b7ec4747488bf1fde9
ucarsystem <gyemsnet@gmail.com>
2025-06-18 12:59:46 +0900

    구성변경

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if 조회버튼 and user_input:
         ### 1. 전체 지표 ###
         driver_name = df_id_check[df_id_check['ECO관리번호'] == driver_id].iloc[0]['성명']
 
-        st.subheader(f"📌{driver_name}님의 전체 주행 지표")
+        st.subheader(f"📌{driver_name}님의 전체 주행 지표📌")
         tang_filtered = df_tang[df_tang['운전자번호'] == driver_id].fillna('')
         driver_info = df_driver[df_driver['운전자ID'] == driver_id].fillna('')
 
